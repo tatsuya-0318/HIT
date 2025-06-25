e95c661c51c2243d8fb46b6d4022a5f257705555
tatsuya-0318 <146906978+tatsuya-0318@users.noreply.github.com>
2025-06-26 01:49:06 +0900

    カリキュラムAのパッケージとクラス作成。

---
+++ HelloWorld_MoriTatsuya/src/curriculum_A/Qes1_13.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package curriculum_A;
+
+/**
+ * 
+ */
+public class Qes1_13 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+
+	}
+
+}
