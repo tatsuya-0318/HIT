e7776097f7b4f1938b44e9669cd178106d7dcb9d
tatsuya-0318 <146906978+tatsuya-0318@users.noreply.github.com>
2025-06-28 01:24:47 +0900

    カリキュラムA1-1完了。

--- HelloWorld_MoriTatsuya/src/curriculum_A/Qes1_13.java
+++ HelloWorld_MoriTatsuya/src/curriculum_A/Qes1_13.java
@@ -13,6 +13,44 @@ public class Qes1_13 {
 	 */
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
+		
+		byte age = 25;
+		// バイト型（-128～127）
+		
+		short temperature = 120;
+		// 短整数型（-32，768～32，767）
+		
+		int score = 85000;
+		// 整数型（-2，147，483，648～2，147，483，647）
+		
+		long population = 125000000L;
+		// 長整数型（-9,223,372,036,854,775,808 ～ 9,223,372,036,854,775,807）
+		
+		float height = 170.5f;
+		// 単精度浮動小数点数型
+		
+		double weight = 65.3;
+		// 倍精度浮動小数点数型
+		
+		char letter = 'A';
+		// 文字型（1文字）
+		
+		String message = "こんにちは";
+		// 文字列型（文字の並び）
+		
+		boolean isActive = true;
+		// ブーリアン型（true or false）
+		
+		System.out.println("バイト型："+age);
+		System.out.println("短整数型："+temperature);
+		System.out.println("整数型"+score);
+		System.out.println("長整数型"+population);
+		System.out.println("単精度浮動小数点数型："+height);
+		System.out.println("倍精度浮動小数点数型"+weight);
+		System.out.println("文字列"+letter);
+		System.out.println("文字列型"+message);
+		System.out.println("ブーリアン型"+isActive);
+		
 
 	}
 
