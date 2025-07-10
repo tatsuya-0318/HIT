5eee2055f16ddcf80db3ac2baa50b4294ab1fc92
tatsuya-0318 <146906978+tatsuya-0318@users.noreply.github.com>
2025-07-10 23:01:14 +0900

    変数の宣言行いました。

--- HelloWorld_MoriTatsuya/src/curriculum_A/Qes1_13.java
+++ HelloWorld_MoriTatsuya/src/curriculum_A/Qes1_13.java
@@ -14,43 +14,34 @@ public class Qes1_13 {
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
 		
-		byte age = 25;
+		byte age ;
 		// バイト型（-128～127）
 		
-		short temperature = 120;
+		short temperature ;
 		// 短整数型（-32，768～32，767）
 		
-		int score = 85000;
+		int score ;
 		// 整数型（-2，147，483，648～2，147，483，647）
 		
-		long population = 125000000L;
+		long population ;
 		// 長整数型（-9,223,372,036,854,775,808 ～ 9,223,372,036,854,775,807）
 		
-		float height = 170.5f;
+		float height ;
 		// 単精度浮動小数点数型
 		
-		double weight = 65.3;
+		double weight ;
 		// 倍精度浮動小数点数型
 		
-		char letter = 'A';
+		char letter ;
 		// 文字型（1文字）
 		
-		String message = "こんにちは";
+		String message ;
 		// 文字列型（文字の並び）
 		
-		boolean isActive = true;
+		boolean isActive ;
 		// ブーリアン型（true or false）
 		
-		System.out.println("バイト型："+age);
-		System.out.println("短整数型："+temperature);
-		System.out.println("整数型"+score);
-		System.out.println("長整数型"+population);
-		System.out.println("単精度浮動小数点数型："+height);
-		System.out.println("倍精度浮動小数点数型"+weight);
-		System.out.println("文字列"+letter);
-		System.out.println("文字列型"+message);
-		System.out.println("ブーリアン型"+isActive);
-		// 出力
+		//ここまででQ1の内容。
 		
 
 	}
