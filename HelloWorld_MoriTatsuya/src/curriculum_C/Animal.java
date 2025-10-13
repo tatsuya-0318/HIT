895d7a67e9cacaa729b3039385a9750d39c9a4bf
tatsuya-0318 <146906978+tatsuya-0318@users.noreply.github.com>
2025-10-14 08:59:03 +0900

    課題3　完了。

--- HelloWorld_MoriTatsuya/src/curriculum_C/Animal.java
+++ HelloWorld_MoriTatsuya/src/curriculum_C/Animal.java
@@ -1,13 +1,27 @@
 package curriculum_C;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class Animal {
 	
 	public static void main(String[] args) {
 		// Q1：Dogクラスを呼び出して変数に代入し、動物の名前をコンソールに出力してください。
 		
+		Dog dog1 = new Dog();  // 引数なしコンストラクタで作成
+		System.out.println("動物の名前：" + dog1.name);
+		
 		// Q2：Dogクラスを呼び出して変数に代入し、動物の数をコンソールに出力してください。
 		
+		Dog dog2 = new Dog(3); // 3匹の犬として作成
+		System.out.println("動物の数：" + dog2.count);
+		
 		// 現在の日時を「yyyy-MM-dd H:m:s」形式で変数に代入して、コンソールに出力してください
+		
+		LocalDateTime now = LocalDateTime.now();
+		DateTimeFormatter fmt = DateTimeFormatter.ofPattern("yyyy-MM-dd H:m:s");
+		String formatted = now.format(fmt);
+		System.out.println("現在日時：" + formatted);
 		
 	}
 
--- HelloWorld_MoriTatsuya/src/curriculum_C/Dog.java
+++ HelloWorld_MoriTatsuya/src/curriculum_C/Dog.java
@@ -1,15 +1,30 @@
 package curriculum_C;
 
+
+
 	public class Dog {
+		
+
 		
 			// Q1：フィールドに動物の名前の変数を定義してください。
 			
-			// Q2：フィールドに動物の数の変数を定義してください。
+			String name;
+			
+			// Q2：フィールドに動物の数の変数を定義してください
+			
+			int count;
 			
 			// Q3：Q1で作成した変数に「犬」を代入するコンストラクタを作成してください。
 			
+			public Dog() {
+				this.name = "犬";
+			}
+			
 			// Q4：Q2で作成した変数に引数を代入するコンストラクタを作成してください。
+			public Dog(int count) {
+				this.name = "犬";  // 名前は固定
+				this.count = count; // 引数を代入
+			}
 			
 		
-	
 	}
