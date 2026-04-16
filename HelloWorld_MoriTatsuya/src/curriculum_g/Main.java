ee8fe916ef47094dd241968ccf383d34c087b778
tatsuya-0318 <146906978+tatsuya-0318@users.noreply.github.com>
2026-04-16 18:28:20 +0900

    Q2.31 問5完了。

--- HelloWorld_MoriTatsuya/src/curriculum_g/Main.java
+++ HelloWorld_MoriTatsuya/src/curriculum_g/Main.java
@@ -2,11 +2,11 @@ package curriculum_g;
 
 class Main {
 	  public static void main(String[] args) {
-	    Person person1 = new Person("鈴木太郎", 20, 1.7, 60.0); // ← weight追加
+	    Person person1 = new Person("鈴木太郎", 20, 1.7, 60);
 	   
 	    System.out.println(person1.name);
 	    System.out.println(person1.age);
 	    System.out.println(person1.height);
-	    System.out.println(person1.weight); // ← 追加
+	    System.out.println(person1.weight);
 	  }
 }
