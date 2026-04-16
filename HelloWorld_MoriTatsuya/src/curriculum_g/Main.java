830938fd95867edde12d235afc88f72e1f24e8dd
tatsuya-0318 <146906978+tatsuya-0318@users.noreply.github.com>
2026-04-16 18:54:47 +0900

    Q2.31 問10完了。

--- HelloWorld_MoriTatsuya/src/curriculum_g/Main.java
+++ HelloWorld_MoriTatsuya/src/curriculum_g/Main.java
@@ -1,10 +1,13 @@
 package curriculum_g;
 
 class Main {
-	  public static void main(String[] args) {
+  public static void main(String[] args) {
 
-	    Person person1 = new Person("鈴木太郎", 20, 1.7, 60);
+    Person person1 = new Person("鈴木太郎", 20, 1.7, 60);
 
-	    person1.print();
-	  }
+    person1.print();
+
+    // ★ 最終問題
+    System.out.println("合計" + Person.count + "人です");
+  }
 }
--- HelloWorld_MoriTatsuya/src/curriculum_g/Person.java
+++ HelloWorld_MoriTatsuya/src/curriculum_g/Person.java
@@ -2,27 +2,30 @@ package curriculum_g;
 
 class Person {
 
-	  String name;
-	  int age;
-	  double height;
-	  double weight;
+  String name;
+  int age;
+  double height;
+  double weight;
 
-	  Person(String name, int age, double height, double weight) {
-	    this.name = name;
-	    this.age = age;
-	    this.height = height;
-	    this.weight = weight;
-	  }
+  // ★ 人数カウント（static）
+  static int count = 0;
 
-	  // BMI
-	  double bmi() {
-	    return this.weight / (this.height * this.height);
-	  }
+  Person(String name, int age, double height, double weight) {
+    this.name = name;
+    this.age = age;
+    this.height = height;
+    this.weight = weight;
 
-	  // ★ 問題9
-	  void print() {
-		  System.out.println("名前は" + this.name + "です");
-		  System.out.println("年は" + this.age + "です");
-		  System.out.println("BMIは" + String.format("%.2f", this.bmi()) + "です");
-	}
+    count++; // ★ 作られるたびに増える
+  }
+
+  double bmi() {
+    return this.weight / (this.height * this.height);
+  }
+
+  void print() {
+    System.out.println("名前は" + this.name + "です");
+    System.out.println("年は" + this.age + "です");
+    System.out.println("BMIは" + String.format("%.2f", this.bmi()) + "です");
+  }
 }
