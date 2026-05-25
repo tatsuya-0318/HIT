d91951950e0af90d2375c33d96a96bd30a25145d
tatsuya-0318 <146906978+tatsuya-0318@users.noreply.github.com>
2026-05-25 21:13:34 +0900

    Q33_3完了。

--- HelloWorld_MoriTatsuya/src/curriculum_i/Person.java
+++ HelloWorld_MoriTatsuya/src/curriculum_i/Person.java
@@ -2,15 +2,17 @@ package curriculum_i;
 
 public class Person {
 
+  // クラスフィールド
   private static int count = 0;
 
+  // インスタンスフィールド
   private String firstName;
   private String lastName;
   private int age;
   private double height;
   private double weight;
 
-  // ★ ① フルバージョン（既存）
+  // ★ コンストラクタ①（フルネーム）
   public Person(String firstName, String lastName, int age, double height, double weight) {
     this.firstName = firstName;
     this.lastName = lastName;
@@ -18,34 +20,38 @@ public class Person {
     this.height = height;
     this.weight = weight;
 
-    count++;
+    Person.count++;
   }
 
-  // ★ ② 追加（問題2）
+  // ★ コンストラクタ②（オーバーロード）
   public Person(String firstName, int age, double height, double weight) {
     this.firstName = firstName;
-    this.lastName = ""; // ← lastNameなし
+    this.lastName = ""; // lastNameなし
     this.age = age;
     this.height = height;
     this.weight = weight;
 
-    count++;
+    Person.count++;
   }
 
+  // フルネーム
   public String fullName() {
     return this.firstName + this.lastName;
   }
 
+  // 表示
   public void print() {
     System.out.println("名前は" + this.fullName() + "です");
     System.out.println("年は" + this.age + "です");
   }
 
+  // BMI
   public double bmi() {
     return this.weight / this.height / this.height;
   }
 
+  // 人数表示
   public static void printCount() {
-    System.out.println("合計" + count + "人です");
+    System.out.println("合計" + Person.count + "人です");
   }
 }
